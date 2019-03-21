8e6d6d967c07c8951a99e3714cd7a7716eda60e2
suwanna <student05@Student05.local>
2019-03-21 15:11:57 +0700

    upload1

--- AuthenSNRU/RegisterViewController.swift
+++ AuthenSNRU/RegisterViewController.swift
@@ -68,7 +68,12 @@ class RegisterViewController: UIViewController {
         
     } //myAlert
     
+    func uploadData(name: String,user: String,password: String) -> Void {
+        
+        let uriString:  String = "https://www.androidthai.in.th/snru/addDataSuwanna.php?isAdd=true&Name=\(name)&User=\(user)&Password=\(password)"
+        
     
+    }
     
     
 
